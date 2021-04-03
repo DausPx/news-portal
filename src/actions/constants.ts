f91c29372b26619f53fca8fcc6753fba01691125
DausX <suuad@live.com>
2021-04-03 15:14:50 +0200

    project structure, adding axios and redux

---
+++ src/actions/constants.ts
@@ -0,0 +1,3 @@
+export const actions = {
+    DEFAULT_ACTION: "default_action",
+  }
---
+++ src/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import reducer from "./reducer";
+
+
+const reducers = combineReducers({
+    reducer: reducer,
+});
+
+export default reducers;
